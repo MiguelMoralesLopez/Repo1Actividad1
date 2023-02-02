34b6d4e95fa9e76ccc63eaeb7c90bcb479490bef
Juanjo <sistemasjuanjo@outlook.es>
2023-02-02 23:55:28 +0100

    arreglos a los conflictos

--- ActividadED1/src/juanjo/Juanjo.java
+++ ActividadED1/src/juanjo/Juanjo.java
@@ -10,10 +10,7 @@ public class Juanjo {
 
 	System.out.println("Mis hobbies son : " + hobbie1 + hobbie2 + hobbie3 );
 
-	System.out.println("Mis tres comidas favoritas son: ");
-	System.out.println("las croquetas");
-	System.out.println("Tortilla de patatas");
-	System.out.println("Cualquiera que se haga con puchero");
+	
 
 	System.out.println("Mis tres comidas favoritas son : ");
 	System.out.println("Macarrones");
