01811023b46876be0606478d85e5a447347a37cc
Miguel Morales Lopez <miguel.mlopez07@gmail.com>
2023-02-02 22:56:43 +0100

    Añado hobbies

--- ActividadED1/src/miguel/Miguel.java
+++ ActividadED1/src/miguel/Miguel.java
@@ -5,7 +5,14 @@ public class Miguel {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola soy Miguel");
-
+		System.out.println("Tres de mis hobbies son: ");
+		String hob1 = "Videojuegos";
+		String hob2 = "Juegos de mesa";
+		String hob3 = "Cine";
+		System.out.println(hob1);
+		System.out.println(hob2);
+		System.out.println(hob3);
+		
 	}
 
 }
