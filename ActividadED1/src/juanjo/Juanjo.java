9aa890ec3dc8b1a488a6374f3f41cf2ac5758dbb
Juanjo <sistemasjuanjo@outlook.es>
2023-02-02 22:57:08 +0100

    Añado hobbies a mi proyecto

--- ActividadED1/src/juanjo/Juanjo.java
+++ ActividadED1/src/juanjo/Juanjo.java
@@ -3,7 +3,12 @@ package juanjo;
 public class Juanjo {
 
 	public static void main(String[] args) {
-	System.out.println(" Hola soy Juanjo");
+	System.out.println("Hola soy Juanjo");
+	String hobbie1= "\n *Musica";
+	String hobbie2 = "\n *Videojuegos";
+	String hobbie3 = "\n *Irme de cañing";
+
+	System.out.println("Mis hobbies son : " + hobbie1 + hobbie2 + hobbie3 );
 	}
 
 }
